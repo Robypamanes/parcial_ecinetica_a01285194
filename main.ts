6ffc63bfb04c456ff684d7fd64ab3362e655a7a6
Robypamanes <70342514+Robypamanes@users.noreply.github.com>
2020-09-17 13:45:19 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Ec=")
+    energia_cinetica = masa * (velocidad * velocidad) / 2
+    basic.showNumber(energia_cinetica)
+})
+let energia_cinetica = 0
+let velocidad = 0
+let masa = 0
+basic.showString("A01285194 Roberta Pámanes Garza")
+masa = 17.78
+velocidad = 15
+basic.forever(function () {
+	
+})
